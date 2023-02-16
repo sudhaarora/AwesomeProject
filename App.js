b9f448328b3796bf684f67ecd57df59a9191eebb
sudhaarora <a.1994sudha@gmail.com>
2023-02-16 16:00:47 +0530

    add core component: button, image, imagebackground, statusbar, safeareaView

--- App.js
+++ App.js
@@ -1,49 +1,105 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+  ImageBackground
+} from 'react-native';
 
-export default function App() {
-  const [titleText, setTitleText] = useState("Bird's Nest");
-  const bodyText = "This is not really a bird nests";
-  const onPressedTitle = () => {
-    setTitleText("After pressed Bird's nest!!")
-  }
-  return (
-    <>
-    <View style={styles.container}>
+const Separator = () => <View style={styles.separator} />;
+const image = {uri: 'https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg'};
+  
+const App = () => (
+  
+  <SafeAreaView style={styles.container}>
+  <View style={styles.container}>
       <Text>Hello Sudha Arora!</Text>
       <StatusBar style="auto" />
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+      <Text style={styles.text}>Inside</Text>
+    </ImageBackground>
     </View>
     <View>
-      <Text style={styles.baseText} onPress={onPressedTitle}>
-        <Text style={styles.titleText}>{titleText}</Text>
+      <Text style={styles.title}>
+        The title and onPress handler are required. It is recommended to set
+        accessibilityLabel to help make your app usable by everyone.
       </Text>
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
     </View>
-    <View style={{
-        flexDirection: 'row',
-        height: 100,
-        padding: 20,
-      }}>
-        <View style={{backgroundColor:'blue', flex:0.3}} />
-        <View style={{backgroundColor:'red', flex:0.5}} />
-        <Text>First React Native project</Text>
+    <Separator />
+    <View>
+      <Text style={styles.title}>
+        Adjust the color in a way that looks standard on each platform. On iOS,
+        the color prop controls the color of the text. On Android, the color
+        adjusts the background color of the button.
+      </Text>
+      <Button
+        title="Press me"
+        color="#f194ff"
+        onPress={() => Alert.alert('Button with adjusted color pressed')}
+      />
+    </View>
+    <Separator />
+    <View>
+      <Text style={styles.title}>
+        All interaction for the component are disabled.
+      </Text>
+      <Button
+        title="Press me"
+        disabled
+        onPress={() => Alert.alert('Cannot press this one')}
+      />
+    </View>
+    <Separator />
+    <View>
+      <Text style={styles.title}>
+        This layout strategy lets the title define the width of the button.
+      </Text>
+      <View style={styles.fixToText}>
+        <Button
+          title="Left button"
+          onPress={() => Alert.alert('Left button pressed')}
+        />
+        <Button
+          title="Right button"
+          onPress={() => Alert.alert('Right button pressed')}
+        />
       </View>
-</>
-  );
-}
+    </View>
+  </SafeAreaView>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  image: {
+    width: '200px',
+    height: '200px',
     justifyContent: 'center',
   },
-  baseText: {
-    fontFamily: 'Cochin',
-  },
-  titleText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  }
 });
+
+export default App;
