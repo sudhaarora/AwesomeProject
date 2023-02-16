602445d451468f914d5dab4062b769fc3275d1ae
sudhaarora <a.1994sudha@gmail.com>
2023-02-16 13:19:39 +0530

    use View, Text, apply css stylesheet,onPressed event, run with android using expo

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [titleText, setTitleText] = useState("Bird's Nest");
+  const bodyText = "This is not really a bird nests";
+  const onPressedTitle = () => {
+    setTitleText("After pressed Bird's nest!!")
+  }
   return (
+    <>
     <View style={styles.container}>
       <Text>Hello Sudha Arora!</Text>
       <StatusBar style="auto" />
     </View>
+    <View>
+      <Text style={styles.baseText} onPress={onPressedTitle}>
+        <Text style={styles.titleText}>{titleText}</Text>
+      </Text>
+    </View>
+    <View style={{
+        flexDirection: 'row',
+        height: 100,
+        padding: 20,
+      }}>
+        <View style={{backgroundColor:'blue', flex:0.3}} />
+        <View style={{backgroundColor:'red', flex:0.5}} />
+        <Text>First React Native project</Text>
+      </View>
+</>
   );
 }
 
@@ -17,4 +39,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
 });
